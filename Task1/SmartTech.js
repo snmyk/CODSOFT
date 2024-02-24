cafcecb5e9976150871c6221211445a0131adff5
NYNSPH001 <nynsph001@myuct.ac.za>
2024-02-24 15:33:24 +0200

    'Added animation to intro page'

--- Task1/SmartTech.js
+++ Task1/SmartTech.js
@@ -1,6 +1,9 @@
 const defaultMenu = document.getElementById("menu-container");
 const sidebar = document.getElementById("sidebar");
 const sidebarIcon = document.getElementById("close-container");
+const introP1 = document.getElementById("intro-p1");
+const introP2 = document.getElementById("intro-p2");
+const introh3 = document.getElementById("intro-h3");
 
 const displaySidebar = () =>
 {
@@ -10,3 +13,12 @@ const hideSidebar = () =>
 {
     sidebar.setAttribute('style', 'display:none');
 }
+setTimeout(() => {
+    introh3.removeAttribute('style');
+}, 600);
+setTimeout(() => {
+    introP1.removeAttribute('style');
+}, 1200);
+setTimeout(() => {
+    introP2.removeAttribute('style');
+}, 1800);
